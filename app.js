5abe63370254f5e2d9f85b25c6fc5bd5a4db97ee
hayleamlak <hayleamlaktesfaye12@gmail.com>
2025-08-15 11:43:06 +0300

    thsi is second code

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end('Home Page');
-    } else if (req.url === '/about') {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end('About Page');
-    } else {
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end('Page Not Found');
+    if (req.url === '/api/notes') {
+        const notes = [
+            { id: 1, title: 'Shopping' },
+            { id: 2, title: 'Reminder' }
+        ];
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(notes));
     }
 });
 
-server.listen(3000, () => console.log('Server running on http://localhost:3000'));
+server.listen(4000, () => console.log('Server running on port 4000'));
